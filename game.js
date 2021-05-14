12bbeb35d5d82952adf0c2f65d41e32e5f57de4e
sarayu96 <sarayu96@yahoo.com>
2021-05-14 17:43:54 +0300

    Show the sequence to the userwith animations

--- game.js
+++ game.js
@@ -1,10 +1,18 @@
-let buttonColours = ["red", "blue", "green", "yellow"];
+let buttonColours = ["red", "blue", "green", "magenta"];
 
 let gamePattern = [];
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
-  let randomChosenColor = buttonColours[nextSequence()];
-  gamePattern.push(randomChosenColor);
-  return gamePattern;
+  return randomNumber;
 }
+
+let randomChosenColor = buttonColours[nextSequence()];
+
+// $("#magenta").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+// $("#red").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+// $("#blue").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+// $("#green").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+$("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+gamePattern.push(randomChosenColor);
