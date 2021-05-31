9501f1cde88dabe52e1f9db5dddcc377f0d4f572
sarayu96 <sarayu96@yahoo.com>
2021-05-31 22:36:33 +0300

    Implemented user clicked pattern

--- game.js
+++ game.js
@@ -2,15 +2,24 @@ let buttonColours = ["red", "blue", "green", "magenta"];
 
 let gamePattern = [];
 
+let userClickedPattern = [];
+
 function nextSequence() {
 
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColours[randomNumber];
-
+  gamePattern.push(randomChosenColor);
 
   $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-  gamePattern.push(randomChosenColor);
 
   let audio = new Audio('sounds/' + randomChosenColor + '.mp3');
   audio.play();
 }
+//
+
+$(".btn").click(function(){
+  userChosenColour = this.id;
+  userClickedPattern.push(userChosenColour);
+  });
+
+console.log(userClickedPattern);
