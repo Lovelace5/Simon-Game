7fd20db67a97b1f0c511441cbde8574913b59e54
sarayu96 <sarayu96@yahoo.com>
2021-05-31 18:33:39 +0300

    Refactor the code

--- game.js
+++ game.js
@@ -3,22 +3,14 @@ let buttonColours = ["red", "blue", "green", "magenta"];
 let gamePattern = [];
 
 function nextSequence() {
+
   let randomNumber = Math.floor(Math.random() * 4);
-  return randomNumber;
-}
+  let randomChosenColor = buttonColours[randomNumber];
 
-let randomChosenColor = buttonColours[nextSequence()];
 
-// $("#magenta").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-// $("#red").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-// $("#blue").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-// $("#green").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  gamePattern.push(randomChosenColor);
 
-$("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-gamePattern.push(randomChosenColor);
-
-let audio = new Audio('sounds/' + randomChosenColor + '.mp3');
-//
-// function playAudio() {
+  let audio = new Audio('sounds/' + randomChosenColor + '.mp3');
   audio.play();
-// }
+}
